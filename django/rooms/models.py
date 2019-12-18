88348ef9f91195743ba9866cfb4d15a2c422f6a5
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2019-12-18 20:05:38 +0200

    Add ability to create rooms with YT videos

--- django/rooms/models.py
+++ django/rooms/models.py
@@ -7,7 +7,8 @@ from autoslug import AutoSlugField
 class Room(models.Model):
     name = models.SlugField(max_length=100, unique=True)
     slug = AutoSlugField(max_length=100, unique=True, populate_from='name')
-    video = models.FileField(upload_to='videos/')
+    video = models.FileField(upload_to='videos/', blank=True, null=True)
+    youtube_link = models.URLField(blank=True, null=True)
     author = models.ForeignKey(
         get_user_model(),
         related_name='room',
