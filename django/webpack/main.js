34d4b7823b5d1e425f83fba17a655c0a036c58d9
Yevhen Shymotyuk <fly1ngDream@pm.me>
2019-12-29 23:05:00 +0200

    Add HLS streaming for local videos

--- django/webpack/main.js
+++ django/webpack/main.js
@@ -1,9 +1,9 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
 
-import 'jquery';
+import "jquery";
