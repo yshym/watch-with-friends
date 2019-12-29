0d7dac325ac35cc5a0d07410b58ec0c1296be8c3
Yevhen Shymotyuk <fly1ngDream@pm.me>
2019-12-29 13:50:30 +0200

    Update production configuration

--- django/web_entrypoint_prod.sh
+++ django/web_entrypoint_prod.sh
@@ -3,5 +3,9 @@
 # Migrate DB
 python manage.py migrate
 
+# Collect static files
+python manage.py collectstatic --no-input
+
 # Run server
 python manage.py runserver 0.0.0.0:8000
+# gunicorn config.wsgi -b 0.0.0.0:8000
