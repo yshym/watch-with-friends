62f71f3e09a020a4ba44e60d31d64e0502687892
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2019-12-29 13:50:30 +0200

    Update production configuration

--- django/config/urls.py
+++ django/config/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('', include('rooms.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
