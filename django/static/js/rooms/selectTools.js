8b492344495878760fdcf53f367a0975edbc4172
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2020-01-02 18:15:53 +0200

    Remove unnecessary JS files

--- django/static/js/rooms/selectTools.js
+++
@@ -1,7 +0,0 @@
-export function selectedText(selectElement) {
-    return selectElement.options[selectElement.selectedIndex].textContent
-}
-
-export function selectedValue(selectElement) {
-    return selectElement.options[selectElement.selectedIndex].value
-}
