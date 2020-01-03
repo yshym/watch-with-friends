9ee9c35f3904695d7a9a64d7df55744ac57e14eb
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2020-01-03 21:39:37 +0200

    Remove video files on room deletion

--- django/rooms/signals.py
+++ django/rooms/signals.py
@@ -1,10 +1,16 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 
 from .models import Room
 from .tasks import convert_for_hls
 
 
-def post_save_file_receiver(sender, instance, *args, **kwargs):
+def post_save_room_receiver(sender, instance, *args, **kwargs):
+    # convert video file to m3u8 format
     convert_for_hls.delay(video_path=instance.video.path)
 
-post_save.connect(post_save_file_receiver, sender=Room)
+def pre_delete_room_receiver(sender, instance, *args, **kwargs):
+    instance.remove_video_files()
+
+
+post_save.connect(post_save_room_receiver, sender=Room)
+pre_delete.connect(pre_delete_room_receiver, sender=Room)
