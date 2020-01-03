8d87241a81345c987a5b81887b2af7e79233b0b6
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2020-01-03 20:14:00 +0200

    Update video validator to support .m3u8 extension

--- django/rooms/validators.py
+++ django/rooms/validators.py
@@ -2,6 +2,6 @@ def validate_video_extension(value):
     import os
     from django.core.exceptions import ValidationError
     ext = os.path.splitext(value.name)[1]
-    valid_extensions = ['.mp4', '.webm', '.ogg']
+    valid_extensions = ['.mp4', '.webm', '.ogg', '.m3u8']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
