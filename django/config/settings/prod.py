d0849b7602d9c7218adffef9c0066363fa6f460f
Yevhen Shymotyuk <fly1ngDream@pm.me>
2020-01-24 21:56:59 +0200

    Add dj-database-url

--- django/config/settings/prod.py
+++ django/config/settings/prod.py
@@ -28,3 +28,9 @@ DATABASES = {
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
+# Heroku
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
