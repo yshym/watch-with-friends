5ed4195943cdc082845cb4d4663cf025618e1b33
Yevhen Shymotyuk <fly1ngDream@pm.me>
2020-01-29 19:17:16 +0200

    Remove unnecessary templatetag

--- django/rooms/templatetags/room_exists.py
+++
@@ -1,9 +0,0 @@
-from django.template.defaulttags import register
-
-
-from ..models import Room
-
-
-@register.filter
-def room_exists(room_name):
-    return Room.objects.filter(name=room_name).exists()
