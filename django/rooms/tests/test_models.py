01a2bd169775812182d33e7ee86666f7967afcb0
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2020-01-29 06:11:03 +0200

    Update model tests of the rooms application

--- django/rooms/tests/test_models.py
+++ django/rooms/tests/test_models.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from ..models import Room
+from ..models import Room, Message
 
 
-class RoomModelsTestCase(TestCase):
+class RoomModelTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -29,3 +29,32 @@ class RoomModelsTestCase(TestCase):
         self.assertEqual(room.author, self.user)
         self.assertEqual(room.youtube_link, youtube_link)
         self.assertEqual(room.video, None)
+
+
+class MessageModelTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        User = get_user_model()
+        cls.user = User.objects.create(
+            username='testuser1',
+            password='testpass123',
+        )
+
+        cls.room = Room.objects.create(
+            name='room1',
+            author=cls.user,
+            youtube_link='https://www.youtube.com/watch?v=1cQh1ccqu8M',
+        )
+
+    def test_create_message(self):
+        message = Message.objects.create(
+            content='123',
+            author=self.user,
+            room=self.room,
+        )
+
+        self.assertEqual(message.content, '123')
+        self.assertEqual(message.author, self.user)
+        self.assertEqual(message.room, self.room)
