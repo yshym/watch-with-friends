aa9d458a7f7101989e6b6820232b343e34c4fb57
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2020-01-29 13:47:49 +0200

    Remove unnecessary Celery debug task

--- django/config/celery.py
+++ django/config/celery.py
@@ -14,8 +14,3 @@ app = Celery(
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request}')
