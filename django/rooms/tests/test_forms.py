8cf6be01982d389e9fe827b7aafa1d031ffe8c38
Yevhen Shymotyuk <fly1ngDream@pm.me>
2020-02-03 20:31:58 +0200

    Add rooms forms tests

---
+++ django/rooms/tests/test_forms.py
@@ -0,0 +1,45 @@
+from django.test import TestCase, SimpleTestCase, Client
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from ..forms import RoomCreateForm
+
+import os
+
+
+class RoomCreateFormTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.username = 'testuser1'
+        cls.password = 'testpass123'
+
+        User = get_user_model()
+        cls.user = User(username=cls.username, password=cls.password,)
+        cls.user.set_password(cls.password)
+        cls.user.save()
+
+    def test_valid_form_with_youtube_video(self):
+        form = RoomCreateForm(
+            data={
+                'name': 'room1',
+                'video_type': 'yt',
+                'youtube_link': 'https://www.youtube.com/watch?v=1cQh1ccqu8M',
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+
+    def test_valid_form_with_local_video(self):
+        upload_video_file = open('media/testfiles/video.mp4', 'rb')
+        video = SimpleUploadedFile(
+            upload_video_file.name, upload_video_file.read()
+        )
+
+        form = RoomCreateForm(
+            data={'name': 'room1', 'video_type': 'local',},
+            files={'video': video,},
+        )
+
+        self.assertTrue(form.is_valid())
