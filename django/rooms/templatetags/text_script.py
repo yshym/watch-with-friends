007fe8337a7655dd8a4837ab56ceb19f0c437552
yevhenshymotiuk <yevhenshymotiuk@gmail.com>
2020-02-17 00:54:50 +0200

    Use custom templatetag for passing variables via script tag

---
+++ django/rooms/templatetags/text_script.py
@@ -0,0 +1,12 @@
+from django.template.defaulttags import register
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
+
+@register.filter
+def text_script(value, element_id):
+    return format_html(
+        '<script id="{}" type="text">{}</script>',
+        element_id,
+        mark_safe(value),
+    )
