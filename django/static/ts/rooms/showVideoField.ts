dd1bf9c7446caffbd45a0656c615b0a55af81b52
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2020-02-16 19:33:50 +0200

    Rewrite roomCreate and showVideoField using TS

---
+++ django/static/ts/rooms/showVideoField.ts
@@ -0,0 +1,21 @@
+import {selectedValue} from "./selectTools"
+
+
+export default function showVideoField() {
+    let videoTypeSelectElement = document.getElementById("id_video_type")
+    let divIdVideo = document.getElementById("div_id_video")
+    let divIdYoutubeLink = document.getElementById("div_id_youtube_link")
+
+    let videoType = selectedValue(videoTypeSelectElement)
+
+    switch (videoType) {
+        case "local": {
+            divIdYoutubeLink.style.display = "none"
+            divIdVideo.style.display = "block"
+        }
+        case "yt": {
+            divIdVideo.style.display = "none"
+            divIdYoutubeLink.style.display = "block"
+        }
+    }
+}
