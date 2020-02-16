ff74da698078ef4f715e08d51d6cd61ed734dfaa
Yevhen Shymotyuk <fly1ngDream@pm.me>
2020-02-16 19:28:42 +0200

    Rewrite index using TS

---
+++ django/static/ts/rooms/index.ts
@@ -0,0 +1,22 @@
+import AlertMessage from "./AlertMessage";
+import {selectedText, selectedValue} from "./selectTools";
+
+
+let roomNameInput = document.getElementsByTagName("select")[0]
+let roomNameSubmitButton = document.getElementById("room-name-submit")
+let container = document.getElementsByClassName("container-xl")[0]
+
+roomNameSubmitButton.onclick = (_e) => {
+    let roomName = selectedText(roomNameInput)
+    let roomValue = selectedValue(roomNameInput)
+    if (roomName.trim()) {
+        window.location.pathname = `/${roomValue}/`
+    } else {
+        let alertMessage = new AlertMessage(
+            container,
+            "Input the name of the room you want to enter!",
+            "warning",
+        )
+        alertMessage.post()
+    }
+}
