4186ac3646932d19622fc159f093c5430f6ba949
yevhenshymotiuk <yevhenshymotiuk@pm.me>
2020-02-17 00:39:34 +0200

    Refactor some TS files

--- django/static/ts/rooms/video.ts
+++ django/static/ts/rooms/video.ts
@@ -1,39 +1,43 @@
 // Initialize Plyr player
 let controls: string[], clickToPlay: boolean;
 
-if (user == roomAuthor) {
-    controls = [
-        "play-large",
-        "play",
-        "progress",
-        "current-time",
-        "mute",
-        "volume",
-        "captions",
-        "airplay",
-        "fullscreen",
-    ];
-    clickToPlay = true;
-} else {
-    controls = [
-        "current-time",
-        "mute",
-        "volume",
-        "captions",
-        "airplay",
-        "fullscreen",
-    ];
-    clickToPlay = false;
-}
+export function initializeVideo(user: string, roomAuthor: string) {
+    if (user == roomAuthor) {
+        controls = [
+            "play-large",
+            "play",
+            "progress",
+            "current-time",
+            "mute",
+            "volume",
+            "captions",
+            "airplay",
+            "fullscreen",
+        ];
+        clickToPlay = true;
+    } else {
+        controls = [
+            "current-time",
+            "mute",
+            "volume",
+            "captions",
+            "airplay",
+            "fullscreen",
+        ];
+        clickToPlay = false;
+    }
 
-export const video = new Plyr("#video-active", {
-    controls: controls,
-    clickToPlay: clickToPlay,
-    fullscreen: {
-        iosNative: true,
-    },
-    youtube: {
-        cc_load_policy: 3,
-        playsinline: true,
-    },
-});
+    const video = new Plyr("#video-active", {
+        controls: controls,
+        clickToPlay: clickToPlay,
+        fullscreen: {
+            iosNative: true,
+        },
+        youtube: {
+            cc_load_policy: 3,
+            playsinline: true,
+        },
+    });
+
+    return video;
+}
