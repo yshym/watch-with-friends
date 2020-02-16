c5b42e480e7a56e5125f116d9328b697a85dc2de
yevhenshymotiuk <yevhenshymotiuk@gmail.com>
2020-02-16 19:33:50 +0200

    Rewrite roomCreate and showVideoField using TS

---
+++ django/static/ts/rooms/roomCreate.ts
@@ -0,0 +1,10 @@
+import showVideoField from "./showVideoField"
+
+
+let nameInput = document.getElementById("id_name")
+let videoTypeSelectElement = document.getElementById("id_video_type")
+
+nameInput.focus()
+
+showVideoField()
+videoTypeSelectElement.onchange = showVideoField
