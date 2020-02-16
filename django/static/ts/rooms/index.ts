170a7426540a651afb2b392004a0be86a8ca5fa1
yevhenshymotiuk <yevhenshymotiuk@gmail.com>
2020-02-16 20:01:20 +0200

    Refactor index, roomCreate and showVideoField

--- django/static/ts/rooms/index.ts
+++ django/static/ts/rooms/index.ts
@@ -1,22 +1,21 @@
 import AlertMessage from "./AlertMessage";
-import {selectedText, selectedValue} from "./selectTools";
+import { selectedText, selectedValue } from "./selectTools";
 
+let roomNameInput = document.getElementsByTagName("select")[0];
+let roomNameSubmitButton = document.getElementById("room-name-submit");
+let container = document.getElementsByClassName("container-xl")[0];
 
-let roomNameInput = document.getElementsByTagName("select")[0]
-let roomNameSubmitButton = document.getElementById("room-name-submit")
-let container = document.getElementsByClassName("container-xl")[0]
-
-roomNameSubmitButton.onclick = (_e) => {
-    let roomName = selectedText(roomNameInput)
-    let roomValue = selectedValue(roomNameInput)
+roomNameSubmitButton.onclick = _e => {
+    let roomName = selectedText(roomNameInput);
+    let roomValue = selectedValue(roomNameInput);
     if (roomName.trim()) {
-        window.location.pathname = `/${roomValue}/`
+        window.location.pathname = `/${roomValue}/`;
     } else {
         let alertMessage = new AlertMessage(
             container,
             "Input the name of the room you want to enter!",
-            "warning",
-        )
-        alertMessage.post()
+            "warning"
+        );
+        alertMessage.post();
     }
-}
+};
