0c255b740a18dabb2fc5c543e16e2c89d8c8b6b0
yevhenshymotiuk <yevhenshymotiuk@gmail.com>
2020-02-17 00:39:34 +0200

    Refactor some TS files

--- django/static/ts/rooms/websockets.ts
+++ django/static/ts/rooms/websockets.ts
@@ -1,102 +1,105 @@
-import { video } from "./video";
 import ChatMessage from "./ChatMessage";
 import ConnectedUser from "./ConnectedUser";
 
 // Initialize WebSocket
-export const roomSocket = new WebSocket(
-    `ws://${window.location.host}/ws/chat/${roomName}/`
-);
+export function initializeRoomSocket(roomName: string, roomAuthor: string, user: string, video: any): WebSocket {
+    const roomSocket = new WebSocket(
+        `ws://${window.location.host}/ws/chat/${roomName}/`
+    );
 
-let container = document.getElementsByClassName("container-xl")[0];
-let chatLogBody = document.getElementById("chat-log-body");
-let connectedUsersContainer = document.getElementById("connectedUsers");
+    let container = <HTMLElement>document.getElementsByClassName("container-xl")[0];
+    let chatLogBody = document.getElementById("chat-log-body");
+    let connectedUsersContainer = document.getElementById("connectedUsers");
 
-function notNewUserWarningElement(text: string): Element {
-    let warningH1 = document.createElement("h1");
-    let warningText = document.createTextNode(text);
+    function notNewUserWarningElement(text: string): Element {
+        let warningH1 = document.createElement("h1");
+        let warningText = document.createTextNode(text);
 
-    warningH1.appendChild(warningText);
+        warningH1.appendChild(warningText);
 
-    return warningH1;
-}
-
-roomSocket.onmessage = (e: any) => {
-    let data = JSON.parse(e.data);
-    let messageType = data.type;
-
-    // Messages for all users
-    switch (messageType) {
-        case "message": {
-            let username = data.username;
-            let content = data.message;
-            let message = new ChatMessage(chatLogBody, username, content, user);
-
-            message.post();
-
-            chatLogBody.scrollTop = chatLogBody.scrollHeight;
-
-            break;
-        }
-        case "user_connected":
-        case "user_disconnected": {
-            let usernameData = data.username;
-            let isNewUserData = data.is_new_user;
-
-            if (isNewUserData) {
-                let connectedUsersDataSet = new Set(
-                    data.connected_users.split(",")
-                );
-
-                connectedUsersContainer.removeChildren();
-
-                connectedUsersDataSet.forEach(username => {
-                    let connectedUser = new ConnectedUser(
-                        <string>username,
-                        user
-                    );
-                    connectedUser.addToContainer(connectedUsersContainer);
-                });
-            } else if (user == usernameData) {
-                container.removeChildren();
-
-                container.appendChild(
-                    notNewUserWarningElement(
-                        "You are already in this room from the other tab/browser"
-                    )
-                );
-            }
-
-            break;
-        }
-        case "buffering_video": {
-            video.pause();
-
-            break;
-        }
-        case "all_players_buffered": {
-            video.play();
-
-            break;
-        }
+        return warningH1;
     }
-    // Messages for all users, except the room creator
-    if (user != roomAuthor) {
+
+    roomSocket.onmessage = (e: any) => {
+        let data = JSON.parse(e.data);
+        let messageType = data.type;
+
+        // Messages for all users
         switch (messageType) {
-            case "seeked_video": {
-                video.currentTime = data.current_time;
+            case "message": {
+                let username = data.username;
+                let content = data.message;
+                let message = new ChatMessage(chatLogBody, username, content, user);
+
+                message.post();
+
+                chatLogBody.scrollTop = chatLogBody.scrollHeight;
 
                 break;
             }
-            case "pause_video": {
+            case "user_connected":
+            case "user_disconnected": {
+                let usernameData = data.username;
+                let isNewUserData = data.is_new_user;
+
+                if (isNewUserData) {
+                    let connectedUsersDataSet = new Set(
+                        data.connected_users.split(",")
+                    );
+
+                    connectedUsersContainer.removeChildren();
+
+                    connectedUsersDataSet.forEach(username => {
+                        let connectedUser = new ConnectedUser(
+                            <string>username,
+                            roomAuthor
+                        );
+                        connectedUser.addToContainer(connectedUsersContainer);
+                    });
+                } else if (user == usernameData) {
+                    container.removeChildren();
+
+                    container.appendChild(
+                        notNewUserWarningElement(
+                            "You are already in this room from the other tab/browser"
+                        )
+                    );
+                }
+
+                break;
+            }
+            case "buffering_video": {
                 video.pause();
 
                 break;
             }
-            case "play_video": {
+            case "all_players_buffered": {
                 video.play();
 
                 break;
             }
         }
-    }
-};
+        // Messages for all users, except the room creator
+        if (user != roomAuthor) {
+            switch (messageType) {
+                case "seeked_video": {
+                    video.currentTime = data.current_time;
+
+                    break;
+                }
+                case "pause_video": {
+                    video.pause();
+
+                    break;
+                }
+                case "play_video": {
+                    video.play();
+
+                    break;
+                }
+            }
+        }
+    };
+
+    return roomSocket;
+}
