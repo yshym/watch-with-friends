a676d8e3bb298195dda44c16c216674103bb8620
Yevhen Shymotyuk <fly1ngDream@pm.me>
2020-02-17 10:56:38 +0200

    Fix HLSFileWaiter global scope type issue

--- django/static/ts/rooms/HLSFileWaiter.ts
+++ django/static/ts/rooms/HLSFileWaiter.ts
@@ -7,7 +7,7 @@ function URLExists(url: string): boolean {
 
 function waitForHLSFile(url: string): any {
     if (URLExists(url)) {
-        self.postMessage("m3u8 file was created.");
+        (self as any).postMessage("m3u8 file was created.");
     } else {
         setTimeout(waitForHLSFile(url), 1000);
     }
