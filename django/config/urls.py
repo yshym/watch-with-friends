50dd741820ee7724ccce8aa192a3d9bf151a411c
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2021-07-24 22:26:19 +0300

    Update static files management for production

--- django/config/urls.py
+++ django/config/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("", include("rooms.urls")),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# if settings.DEBUG:
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #     import debug_toolbar
 #     urlpatterns += [
 #         path('__debug__/', include(debug_toolbar.urls)),
--- django/web_entrypoint.sh
+++ django/web_entrypoint.sh
@@ -7,5 +7,5 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 
 # Run server
-python manage.py runserver 0.0.0.0:8000
-# gunicorn config.wsgi -b 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
+gunicorn config.wsgi -b 0.0.0.0:8000
