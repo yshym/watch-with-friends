99cfbee485fe4c192e2d6981f506ce34485d00b0
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-07-24 16:17:10 +0300

    Remove Heroku-related stuff

--- django/config/settings/prod.py
+++ django/config/settings/prod.py
@@ -30,10 +30,4 @@ DATABASES = {
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
-# Heroku
-# import dj_database_url
-
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES["default"].update(db_from_env)
-
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
