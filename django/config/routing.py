6f9087f5afb2500dcf9cec8cab608e9dcddaa48b
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-07-25 03:11:34 +0300

    Substitute gunicorn with daphne

--- django/config/routing.py
+++ django/config/routing.py
@@ -1,10 +1,14 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import rooms.routing
+from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
+import rooms.routing
 
 application = ProtocolTypeRouter(
     {
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter(
                 rooms.routing.websocket_urlpatterns,
