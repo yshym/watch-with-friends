4ec7e4777419c8b2e42adc6794b41080c6c9938f
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2021-07-25 15:37:36 +0300

    Fix static file collecting in web entrypoint

--- django/web_entrypoint.sh
+++ django/web_entrypoint.sh
@@ -4,7 +4,7 @@
 python manage.py migrate
 
 # Collect static files
-RUN python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Run server
 # python manage.py runserver 0.0.0.0:8000
