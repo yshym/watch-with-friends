2e9e4ca5920af617f147a62e0c8b7ae4b4c790c5
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2021-07-25 03:11:34 +0300

    Substitute gunicorn with daphne

--- django/web_entrypoint.sh
+++ django/web_entrypoint.sh
@@ -8,4 +8,4 @@ RUN python manage.py collectstatic --no-input
 
 # Run server
 # python manage.py runserver 0.0.0.0:8000
-gunicorn config.wsgi -b 0.0.0.0:8000
+daphne -b 0.0.0.0 -p 8000 config.routing:application
