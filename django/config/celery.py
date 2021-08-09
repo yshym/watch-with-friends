6a006eac57a0f3e21a2ac537b2f3b973302054d6
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2021-08-10 00:06:54 +0300

    Fix redis url in celery config

--- django/config/celery.py
+++ django/config/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-redis_url = "redis://redis?db=0"
+redis_url = "redis://redis/0"
 app = Celery("config", broker=redis_url, backend=redis_url)
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
